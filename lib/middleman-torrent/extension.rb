8c43f0e1e632fd54d321280428ce8f6b74482cb3
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-11-10 20:24:50 -0300

    Working

--- lib/middleman-torrent/extension.rb
+++ lib/middleman-torrent/extension.rb
@@ -2,12 +2,38 @@ module MiddlemanTorrent
   class Extension < Middleman::Extension
     option :tracker, 'udp://tracker.publicbt.com:80', 'A tracker to announce'
     option :file, 'site.torrent', 'Torrent file name'
+    option :name, 'site', 'Torrent name and base directory'
+    option :private, false, 'Make torrent private'
 
     def initialize(app, options_hash={}, &block)
       super
+      require 'mktorrent'
+    end
 
-      app.after_build do |b|
-        binding.pry
+    # Create the torrent after the site is built
+    def after_build(builder)
+      torrent = Torrent.new options.tracker
+
+      torrent.defaultdir = options.name
+      torrent.set_private if options.private
+
+      # Move to build_dir so it's not added to the torrent
+      within_build_path do
+        app.sitemap.resources.each do |file|
+          torrent.add_file file.path
+          builder.say_status 'to torrent', file.path
+        end
+
+        torrent.write_torrent options.file
+      end
+
+      builder.say_status :create, File.join(app.config.build_dir, options.file)
+    end
+
+    # Move to build dir for the block
+    def within_build_path(&block)
+      Dir.chdir app.config.build_dir do
+        yield
       end
     end
   end
